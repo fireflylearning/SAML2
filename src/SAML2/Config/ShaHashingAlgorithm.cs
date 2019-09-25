ec8454339148a73e86fee0dc526961006d4f7d02
Gurpreet Singh Sohal <gurpreet@gurpreetsohal.com>
2019-09-25 14:48:27 +0100

    more whitespace

--- src/SAML2/Config/ShaHashingAlgorithm.cs
+++ src/SAML2/Config/ShaHashingAlgorithm.cs
@@ -1,17 +1,8 @@
 ﻿namespace SAML2.Config {
     public enum ShaHashingAlgorithm
     {
-        /// <summary>
-        /// SHA1
-        /// </summary>
         SHA1,
-        /// <summary>
-        /// SHA256
-        /// </summary>
         SHA256,
-        /// <summary>
-        /// SHA512
-        /// </summary>
         SHA512
     }
 }
